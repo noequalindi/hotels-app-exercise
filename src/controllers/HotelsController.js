981802e0caf59e9361c824d24e4ea03f1fcf7692
noequalindi <noelia.qualindi@gmail.com>
2020-01-12 23:16:05 -0300

    Added other layer of controller

---
+++ src/controllers/HotelsController.js
@@ -0,0 +1,46 @@
+'use strict';
+const data = require('../data/data.json');
+const url = require('url');
+
+async function index (request, response) {
+    
+    const url_parts = url.parse(request.url, true);
+    const query = url_parts.query;
+    let result = data;    
+    
+    let page = parseInt(request.query.p);
+
+    if (!page) page = 1;
+
+    if (query.name) {
+        result = result.filter(item => {
+            return item.name.search((new RegExp(query.name, 'i'))) > -1;
+        }); 
+    }
+    
+    if (query.stars && query.stars.length) {
+        let stars = query.stars
+        .split(';')
+        .map(item => parseInt(item, 10));
+
+        result = result.filter(item => {
+        return stars.includes(item.stars);
+        });
+    }  
+
+    const pageCount = Math.ceil(result.length / 10);
+    if (page > pageCount) {
+        page = pageCount
+    }
+    
+    response.json({
+        "page": page,
+        "pageCount": pageCount,
+        "result": result.slice(page * 10 - 10, page * 10)
+    })
+    
+}
+
+module.exports = {
+    index: index
+};
--- src/routes/hotels.js
+++ src/routes/hotels.js
@@ -1,48 +1,7 @@
 const { Router } = require('express')
 const router = Router();
-const paginate = require('express-paginate');
+hotelsController = require('../controllers/HotelsController');
 
-var data = require('../data/data.json')
-var url = require('url')
-
-router.get('/', async (request, response) => {
-    var url_parts = url.parse(request.url, true);
-    var query = url_parts.query;
-    var result = data;    
-    
-    let page = parseInt(request.query.p);
-
-    if (!page) page = 1;
-
-    if (query.name) {
-        result = result.filter(item => {
-            return item.name.search((new RegExp(query.name, 'i'))) > -1;
-        }); 
-    }
-    
-    if (query.stars && query.stars.length) {
-        let stars = query.stars
-        .split(';')
-        .map(item => parseInt(item, 10));
-
-        result = result.filter(item => {
-        return stars.includes(item.stars);
-        });
-    }  
-
-    const pageCount = Math.ceil(result.length / 10);
-    if (page > pageCount) {
-        page = pageCount
-    }
-    
-    response.json({
-        "page": page,
-        "pageCount": pageCount,
-        "result": result.slice(page * 10 - 10, page * 10)
-    })
-    
-});
-
-
+router.get('/', hotelsController.index)
 
 module.exports = router;
