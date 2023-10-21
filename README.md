2cf50cebbfd84bfb92dc47abb7714717ee4d4860
Noelia Qualindi <noelia.qualindi@gmail.com>
2023-10-21 11:27:51 -0300

    change structure of app

--- README.md
+++
@@ -1,3 +0,0 @@
-# exercise-backend
-- first run #npm install
-- then run node src/index.js
--- src/routes/hotels.js
+++
@@ -1,7 +0,0 @@
-const { Router } = require('express')
-const router = Router();
-const hotelsController = require('../controllers/HotelsController');
-
-router.get('/', hotelsController.index)
-
-module.exports = router;
